539ae6ad0d6afd66452557b2bd4d45b24c425a4c
BudNatsu <44428671+BudNatsu@users.noreply.github.com>
2021-09-08 10:02:29 +0200

    Update docker-compose.sh

--- docker-compose.sh
+++ docker-compose.sh
@@ -5,7 +5,7 @@ echo "Quel nom souhaitez vous utiliser ?"
 read -e nom
 
 
-read -e -p "Où souhaitez-vous créer ce répertoire ?" -i "à modifier" rep
+read -e -p "Où souhaitez-vous créer ce répertoire ?" -i "/root/docker-tomcat/" rep
 
 echo "Création du répertoire $nom dans $rep"
 if [[ -d "$nom" ]]
@@ -21,22 +21,25 @@ dc="$rep/$nom"
 cd $dc
 #Une fois dans le nouveau répertoire, on créer le dossier log, car si on le laisse dans le docker-compose.yml, si on éteint et redemarre le container celui-ci ne se relancera pas à cause du dossier.
 mkdir log
-sudo chmod 644 log
+sudo chmod 774 log
+
+#read -e -p "Chemin du docker compose à utiliser :" -i "/root/docker-tomcat/" docker
+#cp $docker/* $dc
 
 git clone https://github.com/BudNatsu/Tomcat-ref.git
-
+cd Tomcat-ref/
 #cp $docker/docker-compose.yml $dc
 #cp $docker/.env $dc
 #cp $docker/sample.war $dc
-echo "container_name=$nom" >> $dc/.env
+echo "container_name=$nom" >> $dc/Tomcat-ref/.env
 
-read -e -p "Adresse d'elasticsearch :" -i "à modifier" elasticsearch
-echo "ELASTICSEARCH_HOSTS=$elasticsearch:9200" >> $dc/.env
+read -e -p "Adresse d'elasticsearch :" -i "fagonie-bcs.v212.abes.fr" elasticsearch
+echo "ELASTICSEARCH_HOSTS=$elasticsearch:9200" >> $dc/Tomcat-ref/.env
 
-read -e -p "Adresse de kibana :" -i "à modifier" kibana 
-echo "KIBANA_HOST=$kibana:5601" >> $dc/.env
+read -e -p "Adresse de kibana :" -i "fagonie-bcs.v212.abes.fr" kibana 
+echo "KIBANA_HOST=$kibana:5601" >> $dc/Tomcat-ref/.env
 
-read -e -p "Adresse de logstash :" -i "à modifier" kibana 
-echo "LOGSTASH_HOST=$kibana:5044" >> $dc/.env
+read -e -p "Adresse de logstash :" -i "fagonie-bcs.v212.abes.fr" kibana 
+echo "LOGSTASH_HOST=$kibana:5044" >> $dc/Tomcat-ref/.env
 
 docker-compose up -d
