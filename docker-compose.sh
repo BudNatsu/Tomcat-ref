ce656c49ce78d76097f82a3b831e1a6c8e0b95fd
BudNatsu <44428671+BudNatsu@users.noreply.github.com>
2021-09-08 10:09:45 +0200

    Update docker-compose.sh

--- docker-compose.sh
+++ docker-compose.sh
@@ -5,7 +5,7 @@ echo "Quel nom souhaitez vous utiliser ?"
 read -e nom
 
 
-read -e -p "Où souhaitez-vous créer ce répertoire ?" -i "/root/docker-tomcat/" rep
+read -e -p "Où souhaitez-vous créer ce répertoire ?" -i "à modifier" rep
 
 echo "Création du répertoire $nom dans $rep"
 if [[ -d "$nom" ]]
@@ -23,7 +23,7 @@ cd $dc
 mkdir log
 sudo chmod 774 log
 
-#read -e -p "Chemin du docker compose à utiliser :" -i "/root/docker-tomcat/" docker
+#read -e -p "Chemin du docker compose à utiliser :" -i "à modifier" docker
 #cp $docker/* $dc
 
 git clone https://github.com/BudNatsu/Tomcat-ref.git
@@ -33,13 +33,13 @@ cd Tomcat-ref/
 #cp $docker/sample.war $dc
 echo "container_name=$nom" >> $dc/Tomcat-ref/.env
 
-read -e -p "Adresse d'elasticsearch :" -i "fagonie-bcs.v212.abes.fr" elasticsearch
+read -e -p "Adresse d'elasticsearch :" -i "à modifier" elasticsearch
 echo "ELASTICSEARCH_HOSTS=$elasticsearch:9200" >> $dc/Tomcat-ref/.env
 
-read -e -p "Adresse de kibana :" -i "fagonie-bcs.v212.abes.fr" kibana 
+read -e -p "Adresse de kibana :" -i "à modifier" kibana 
 echo "KIBANA_HOST=$kibana:5601" >> $dc/Tomcat-ref/.env
 
-read -e -p "Adresse de logstash :" -i "fagonie-bcs.v212.abes.fr" kibana 
+read -e -p "Adresse de logstash :" -i "à modifier" kibana 
 echo "LOGSTASH_HOST=$kibana:5044" >> $dc/Tomcat-ref/.env
 
 docker-compose up -d
