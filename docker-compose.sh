638eea54d54ac45687a1ebbc05ca9bc06bee844b
BudNatsu <44428671+BudNatsu@users.noreply.github.com>
2021-09-08 16:13:09 +0200

    Update docker-compose.sh

--- docker-compose.sh
+++ docker-compose.sh
@@ -15,31 +15,33 @@ then
 else
     mkdir $rep/$nom 
 fi
-sudo chmod 774 $rep/$nom
+sudo chmod 644 $rep/$nom
 echo $rep/$nom
 dc="$rep/$nom"
 cd $dc
 #Une fois dans le nouveau répertoire, on créer le dossier log, car si on le laisse dans le docker-compose.yml, si on éteint et redemarre le container celui-ci ne se relancera pas à cause du dossier.
-mkdir log
-sudo chmod 774 log
+mkdir -p /opt/pod/$nom/log
+sudo chmod 644 /opt/pod/$nom/log
 
 #read -e -p "Chemin du docker compose à utiliser :" -i "à modifier" docker
 #cp $docker/* $dc
 
 git clone https://github.com/BudNatsu/Tomcat-ref.git
 cd Tomcat-ref/
-#cp $docker/docker-compose.yml $dc
-#cp $docker/.env $dc
-#cp $docker/sample.war $dc
-echo "container_name=$nom" >> $dc/Tomcat-ref/.env
+
+mv * ../
+cd ..
+rm -rf Tomcat-ref/
+
+echo "container_name=$nom" >> $dc/.env
 
 read -e -p "Adresse d'elasticsearch :" -i "à modifier" elasticsearch
-echo "ELASTICSEARCH_HOSTS=$elasticsearch:9200" >> $dc/Tomcat-ref/.env
+echo "ELASTICSEARCH_HOSTS=$elasticsearch:9200" >> $dc/.env
 
 read -e -p "Adresse de kibana :" -i "à modifier" kibana 
-echo "KIBANA_HOST=$kibana:5601" >> $dc/Tomcat-ref/.env
+echo "KIBANA_HOST=$kibana:5601" >> $dc/.env
 
 read -e -p "Adresse de logstash :" -i "à modifier" logstash 
-echo "LOGSTASH_HOST=$logstash:5044" >> $dc/Tomcat-ref/.env
+echo "LOGSTASH_HOST=$logstash:5044" >> $dc/.env
 
 docker-compose up -d
